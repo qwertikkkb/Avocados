3f726795e43ee75fee1591c2125fb5d0bd0671bb
qwertikkkb <147443772+qwertikkkb@users.noreply.github.com>
2024-01-25 20:32:33 +0200

    First commit (Launch Screen)

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Nicolai Bodean on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
